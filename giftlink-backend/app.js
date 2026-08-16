eb7f70e7ff4479212296901a08529029719fe33b
sibankar2314 <sibankar.2314510632@mujonline.edu.in>
2026-08-16 11:35:17 -0400

    Implement search service API

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -1,35 +1,33 @@
+/*jshint esversion: 8 */
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const pinoLogger = require('./logger');
 
 const connectToDatabase = require('./models/db');
-const { loadData } = require('./util/import-mongo/index');
+const {loadData} = require("./util/import-mongo/index");
 
 const app = express();
-app.use('*', cors());
+app.use("*", cors());
 const port = 3060;
 
-// Connect to MongoDB; we just do this one time
 connectToDatabase().then(() => {
     pinoLogger.info('Connected to DB');
-}).catch((e) => console.error('Failed to connect to DB', e));
+})
+    .catch((e) => console.error('Failed to connect to DB', e));
 
 app.use(express.json());
 
-// Route files
 const giftRoutes = require('./routes/giftRoutes');
-
-// Search API tasks are completed in a later lab.
-
+const searchRoutes = require('./routes/searchRoutes');
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
+
 app.use(pinoHttp({ logger }));
 
-// Use Routes
 app.use('/api/gifts', giftRoutes);
+app.use('/api/search', searchRoutes);
 
-// Global Error Handler
 app.use((err, req, res, next) => {
     console.error(err);
     res.status(500).send('Internal Server Error');
@@ -40,5 +38,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+    console.log('Server running on port ' + port);
 });
